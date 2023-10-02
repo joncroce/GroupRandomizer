45c4c1daf129e8dac593c1d5f10e78e1296b8484
Jon Croce <croce.dev@gmail.com>
2023-10-02 04:09:22 -0400

    Implement ideal grouping and refactor UI

--- ViewModels/RosterViewModel.cs
+++ ViewModels/RosterViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using CommunityToolkit.Mvvm.Messaging;
@@ -14,13 +16,15 @@ namespace GroupRandomizer.ViewModels
         private ObservableCollection<string> _rosters;
         private string _selectedRosterName;
         private ObservableCollection<string> _selectedRoster;
+        private ObservableCollection<List<string>> _peopleGroups;
+        private int _groupSize;
         public ObservableCollection<string> Rosters 
         {
             get { return _rosters; }
             set
             {
                 _rosters = value;
-                OnPropertyChanged();
+                OnPropertyChanged(nameof(Rosters));
             }
         }
         public string SelectedRosterName
@@ -29,7 +33,7 @@ namespace GroupRandomizer.ViewModels
             set
             {
                 _selectedRosterName = value;
-                OnPropertyChanged();
+                OnPropertyChanged(nameof(SelectedRosterName));
             }
         }
         public ObservableCollection<string> SelectedRoster
@@ -38,12 +42,38 @@ namespace GroupRandomizer.ViewModels
             set
             {
                 _selectedRoster = value;
-                OnPropertyChanged();
+                OnPropertyChanged(nameof(SelectedRoster));
+                OnPropertyChanged(nameof(GroupButtonIsEnabled));
+            }
+        }
+        public ObservableCollection<List<string>> PeopleGroups
+        {
+            get { return _peopleGroups; }
+            set
+            {
+                _peopleGroups = value;
+                OnPropertyChanged(nameof(PeopleGroups));
+            }
+        }
+        public int GroupSize
+        {
+            get { return _groupSize; }
+            set
+            {
+                if (_groupSize != value)
+                {
+                    _groupSize = value;
+                    OnPropertyChanged(nameof(GroupSize));
+                    OnPropertyChanged(nameof(GroupButtonIsEnabled));
+                }
             }
         }
         public ICommand SelectRosterCommand { get; private set; }
         public ICommand ShowNewRosterPromptCommand { get; private set; }
         public ICommand ShowAddPersonToRosterPromptCommand { get; private set; }
+        public ICommand GroupPeopleCommand { get; private set; }
+
+        public bool GroupButtonIsEnabled => IsValidTargetGroupSize();
 
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
@@ -89,6 +119,9 @@ namespace GroupRandomizer.ViewModels
                     AddPersonToSelectedRoster(args.Result);
                 }
             });
+
+            GroupPeopleCommand = new Command(GroupPeople);
+            GroupSize = 4;
         }
 
         public async Task LoadRostersAsync()
@@ -114,6 +147,68 @@ namespace GroupRandomizer.ViewModels
             }
         }
 
+        private bool IsValidTargetGroupSize()
+        {
+            if (SelectedRoster is null) return false;
+            if (SelectedRoster.Count < 1) return false;
+            if (GroupSize < 2) return false;
+
+            int remainder = SelectedRoster.Count % GroupSize;
+            int minGroupsCount = remainder == 0 ? 0 : GroupSize - remainder;
+            int maxGroupsCount = remainder == 0 ? SelectedRoster.Count / GroupSize : (SelectedRoster.Count - (minGroupsCount * (GroupSize - 1))) / GroupSize;
+
+            if (maxGroupsCount < 1) return false;
+
+            return true;
+        }
+        private void GroupPeople()
+        {
+            if (SelectedRoster is not null)
+            {
+                var people = SelectedRoster.ToList();
+                Shuffle(people);
+
+                int remainder = SelectedRoster.Count % GroupSize;
+                int minGroupsCount = remainder == 0 
+                    ? 0 
+                    : GroupSize - remainder;
+                int maxGroupsCount = remainder == 0 
+                    ? SelectedRoster.Count / GroupSize 
+                    : (SelectedRoster.Count - (minGroupsCount * (GroupSize - 1))) / GroupSize;
+                int groupsCount = minGroupsCount + maxGroupsCount;
+
+                ObservableCollection<List<string>> groups = new();
+                int index = 0;
+                for (int i = 0; i < groupsCount; i++)
+                {
+                    int n = i < maxGroupsCount ? GroupSize : GroupSize - 1;
+                    List<string> group = new();
+
+                    for (int j = 0; j < n; j++)
+                    {
+                        group.Add(people[index]);
+                        index++;
+                    }
+
+                    groups.Add(group);
+                }
+
+                PeopleGroups = groups;
+            }
+        }
+        private static void Shuffle<T>(IList<T> list)
+        {
+            var rng = new Random();
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
         private async void Save()
         {
             await _rosterService.SaveRosterAsync(SelectedRosterName, SelectedRoster.ToList());
